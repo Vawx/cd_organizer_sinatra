3295748d3af1a811bd0fa6b0558484fe4a2ac3d2
Kyle Langley and Patrick Sullivan <altck8@gmail.com>
2016-01-13 13:29:39 -0800

    input for artist available to the user

--- app.rb
+++ app.rb
@@ -5,6 +5,14 @@ require './lib/cd'
 also_reload("lib/**/*.rb")
 
 get '/' do
+  @cd_list = Catalog.get_cds
+  erb :index
+end
 
+post '/add_artist' do
+  @artist_input = params.fetch('artist')
+  cd = CD.new(@artist_input)
+  Catalog.add_cd( cd )
+  @cd_list = Catalog.get_cds
   erb :index
 end
--- lib/catalog.rb
+++ lib/catalog.rb
@@ -8,4 +8,14 @@ class Catalog
   define_singleton_method(:get_cds) do
     return @@cd_list
   end
+
+  define_singleton_method(:get_cds_by_artist) do |artist|
+    found = []
+    @@cd_list.each do |cd|
+      if cd.artist == artist
+        found.push( cd )
+      end
+    end
+    return found
+  end
 end
